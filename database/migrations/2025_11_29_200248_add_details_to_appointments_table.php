0c050f6e2a014c3bde0ce1e119305d591bd99b05
MrSeveco <seveco2247@gmail.com>
2025-11-29 20:04:32 -0500

    Changes users and appointment tables
    Adding details to those two tables

---
+++ database/migrations/2025_11_29_200248_add_details_to_appointments_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('appointments', function (Blueprint $table) {
+            $table->string('patient_id')->nullable()->after('user_id'); // Cedula
+            $table->string('patient_email')->nullable()->after('patient_id');
+            $table->text('appointment_reason')->nullable()->after('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('appointments', function (Blueprint $table) {
+            $table->dropColumn(['patient_id', 'patient_email', 'appointment_reason']);
+        });
+    }
+};
