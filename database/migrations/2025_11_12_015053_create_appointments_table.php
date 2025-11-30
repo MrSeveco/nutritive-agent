518258f8c3c2f827f50ca9a50b18ab7feb22b771
frank.acosta <frank.acosta@newshore.es>
2025-11-29 20:32:32 -0500

    add patient document

--- database/migrations/2025_11_12_015053_create_appointments_table.php
+++ database/migrations/2025_11_12_015053_create_appointments_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('doctor_id')->constrained('users')->onDelete('cascade');
             $table->dateTime('appointment_date');
+            $table->string('patient_document');
             $table->string('status')->default('scheduled');
             $table->string('patient_name');
             $table->string('patient_email');
